8712c6ab9004e8ddac827325b26118bc80073baf
ScottKirvan <scottkirvan@gmail.com>
2023-04-06 13:53:17 -0700

    add build.bat file to build the plugin package

---
+++ build.bat
@@ -0,0 +1,3 @@
+pushd C:\bin\Epic Games\UE_4.27
+call Engine\Build\BatchFiles\RunUAT.bat BuildPlugin -Plugin=D:\3\UE\5.0\dev\ScooterUtils_Base\Plugins\ScooterUtils\ScooterUtils.uplugin -Package=d:\build -Rocket
+popd
