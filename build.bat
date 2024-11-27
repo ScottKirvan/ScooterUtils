d8ba6fe463a18e9bb4e13fdcb2d7bcd3dcba0082
Scott Kirvan <61367618+ScottKirvan@users.noreply.github.com>
2024-11-26 22:38:00 -0800

    update and cleanup built.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,13 @@
-pushd C:\bin\Epic Games\UE_4.27
-call Engine\Build\BatchFiles\RunUAT.bat BuildPlugin -Plugin=D:\3\UE\5.0\dev\ScooterUtils_Base\Plugins\ScooterUtils\ScooterUtils.uplugin -Package=d:\build -Rocket
+set ENGINEDIR=C:\bin\Epic Games\UE_5.5
+set PROJECTDIR=D:\3\UE\5.5\dev\ScooterUtils_Base
+set BUILDDIR=D:\build
+
+pushd "%ENGINEDIR%"
+call Engine\Build\BatchFiles\RunUAT.bat BuildPlugin ^
+-Plugin="%PROJECTDIR%\Plugins\ScooterUtils\ScooterUtils.uplugin" ^
+-Package="%BUILDDIR%\ScooterUtils" ^
+-Rocket
 popd
+
+
+
