402f558a33786fe75fb56723bfc7d1b2abec1098
Scott Kirvan <61367618+ScottKirvan@users.noreply.github.com>
2024-12-11 17:07:47 -0800

    refactored some function names for clarity

--- Source/ScooterUtils/Private/ScooterUtilsMenu.cpp
+++ Source/ScooterUtils/Private/ScooterUtilsMenu.cpp
@@ -20,21 +20,22 @@ public:
 
 	virtual void RegisterCommands() override
 	{
-		UI_COMMAND(MenuCommand1, "Restart Editor...", "Restart the editor, re-opening the current project. - ScooterUtils", EUserInterfaceActionType::Button, FInputChord());
-		UI_COMMAND(MenuCommand2, "Show Project in Explorer", "Find this project on disk. - ScooterUtils", EUserInterfaceActionType::Button, FInputChord());
+		UI_COMMAND(MenuRestartEditor, "Restart Editor...", "Restart the editor, re-opening the current project. - ScooterUtils", EUserInterfaceActionType::Button, FInputChord());
+		UI_COMMAND(MenuShowExplorer, "Show Project in Explorer", "Find this project on disk. - ScooterUtils", EUserInterfaceActionType::Button, FInputChord());
+		// UI_COMMAND(DecreaseBrushFalloff, "Decrease Brush Falloff", "Press this key to decrease brush falloff by a fixed increment.", EUserInterfaceActionType::RadioButton, FInputChord(EModifierKey::Control | EModifierKey::Shift, EKeys::LeftBracket));
 	}
 
 public:
-	TSharedPtr<FUICommandInfo> MenuCommand1;
-	TSharedPtr<FUICommandInfo> MenuCommand2;
+	TSharedPtr<FUICommandInfo> MenuRestartEditor;
+	TSharedPtr<FUICommandInfo> MenuShowExplorer;
 };
 
 void ScooterUtilsMenu::MapCommands()
 {
 	const auto &Commands = ScooterUtilsMenuCommands::Get();
 
-	CommandList->MapAction(Commands.MenuCommand1, FExecuteAction::CreateSP(this, &ScooterUtilsMenu::MenuCommand1), FCanExecuteAction());
-	CommandList->MapAction(Commands.MenuCommand2, FExecuteAction::CreateSP(this, &ScooterUtilsMenu::MenuCommand2), FCanExecuteAction());
+	CommandList->MapAction(Commands.MenuRestartEditor, FExecuteAction::CreateSP(this, &ScooterUtilsMenu::MenuRestartEditor), FCanExecuteAction());
+	CommandList->MapAction(Commands.MenuShowExplorer, FExecuteAction::CreateSP(this, &ScooterUtilsMenu::MenuShowExplorer), FCanExecuteAction());
 }
 
 void ScooterUtilsMenu::OnStartupModule()
@@ -55,18 +56,18 @@ void ScooterUtilsMenu::OnShutdownModule()
 
 void ScooterUtilsMenu::MakeMenuEntry(FMenuBuilder &menuBuilder)
 {
-	menuBuilder.AddMenuEntry(ScooterUtilsMenuCommands::Get().MenuCommand1);
-	menuBuilder.AddMenuEntry(ScooterUtilsMenuCommands::Get().MenuCommand2);
+	menuBuilder.AddMenuEntry(ScooterUtilsMenuCommands::Get().MenuRestartEditor);
+	menuBuilder.AddMenuEntry(ScooterUtilsMenuCommands::Get().MenuShowExplorer);
 }
 
 // Restarts the editor, re-opening the current project
-void ScooterUtilsMenu::MenuCommand1()
+void ScooterUtilsMenu::MenuRestartEditor()
 {
 	FUnrealEdMisc::Get().RestartEditor(false);
 }
 
 // Shows the project folder in explorer/finder
-void ScooterUtilsMenu::MenuCommand2()
+void ScooterUtilsMenu::MenuShowExplorer()
 {
 	FString ProjectFolder = FPaths::ConvertRelativePathToFull(FPaths::ProjectDir());
 	FPlatformProcess::ExploreFolder(*ProjectFolder);
--- Source/ScooterUtils/Public/ScooterUtilsMenu.h
+++ Source/ScooterUtils/Public/ScooterUtilsMenu.h
@@ -19,6 +19,6 @@ protected:
 
 	//************************
 	// UI Command functions
-	void MenuCommand1();  // restart editor
-	void MenuCommand2();  // open project folder
+	void MenuRestartEditor(); // restart editor
+	void MenuShowExplorer();  // open project folder
 };
