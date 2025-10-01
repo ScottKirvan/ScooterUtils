bb6d3b3eec3f86539d76cee3a2c666776d875199
scott kirvan <fork@skvfx.com>
2025-09-30 17:47:36 -0700

    feat: Initial commit of ScooterUtilsBPLibrary
    Adds several blueprint nodes for working with ini files:
    - GetGlobalConfigFileString
    - GetGlobalConfigFileFloat
    - GetGlobalConfigFileInt
    - GetGlobalConfigFileBool
    - SetGlobalConfigFileString
    - SetGlobalConfigFileFloat
    - SetGlobalConfigFileInt
    - SetGlobalConfigFileBool
    Each takes a section and key, and either retrieves or sets the corresponding value in the global ini file.
    Thease are all found under ScooterUtils in the blueprint node menu.

---
+++ Source/ScooterUtils/Private/ScooterUtilsBPLibrary.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ScooterUtilsBPLibrary.h"
+
+UScooterUtilsBPLibrary::UScooterUtilsBPLibrary(const FObjectInitializer &ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
